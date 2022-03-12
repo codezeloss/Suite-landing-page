458fd819abe371338c55fff5d4e734023527322a
Elhoussaine AIT AICHTE <elhoussaineaitX@gmail.com>
2022-03-12 01:53:15 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1180.85px",
+    },
+    colors: {
+      "blue-dark": "#172339",
+      "blue-light": "#49566D",
+      cream: "#F3EDE7",
+      white: "#FAF8F6",
+      purple: "#A060FF",
+      pink: "#CB30E3",
+      orange: "#FFA84E",
+    },
+    fontFamily: {
+      sans: ["Epilogue", "sans-serif"],
+    },
+    extend: {
+      // margin:
+    },
+    plugins: [],
   },
-  plugins: [],
 };
